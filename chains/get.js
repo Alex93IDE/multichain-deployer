e2ebed4c5966c2e946cf1c85f878a24008a9b5bf
Alex93IDE <adsf.arduino@gmail.com>
2022-09-27 01:10:40 -0400

    add network fantom, avalanche and cronos

--- chains/get.js
+++ chains/get.js
@@ -1,9 +1,18 @@
 const polygon = require('./networks/polygon')
 const bsc = require('./networks/bsc')
+const fantom = require('./networks/fantom')
+const avalanche = require('./networks/avalanche')
+const cronos = require('./networks/cronos')
 
 module.exports = {
     polygon: polygon.mainnet,
     polygon_testnet: polygon.testnet,
     bsc: bsc.mainnet,
-    bsc_testnet: bsc.testnet
+    bsc_testnet: bsc.testnet,
+    avalanche: avalanche.mainnet,
+    avalanche_testnet: avalanche.testnet,
+    fantom: fantom.mainnet,
+    fantom_testnet: fantom.testnet,
+    cronos: cronos.mainnet,
+    cronos_testnet: cronos.testnet,
 }
